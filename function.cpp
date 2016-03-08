389d568e8d0ad8effd1fa89866e0009786b451fc
Arthur Goodman <arthur.goodman3@gmail.com>
2016-03-09 00:55:24 +0200

    something

--- function.cpp
+++ function.cpp
@@ -1,4 +1,15 @@
 #include "function.h"
 
+#include <string.h>
+
 namespace vm {
+void Function::gen(const char *opcodes) {
+    int len = strlen(opcodes);
+    byte *data = code.allocate(len);
+    memcpy(data, opcodes, len);
+}
+
+void Function::invoke() {
+    ((void (*)())code.getData())();
+}
 }
--- function.h
+++ function.h
@@ -1,7 +1,15 @@
 #pragma once
 
+#include "bytearray.h"
+
 namespace vm {
 class Function {
 public:
+    ByteArray code;
+
+public:
+    void gen(const char *opcodes);
+
+    void invoke();
 };
 }
